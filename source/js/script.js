dc6bcc569064c5f25d52e0ca9fd502ecbad62fc3
Дмитрий <76389902+BAKERholy@users.noreply.github.com>
2021-05-23 21:34:39 +0300

    module3-task2

---
+++ source/js/script.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector(".nav__toggle");
+const navList = document.querySelector(".nav-list");
+const nav = document.querySelector(".nav");
+
+navToggle.classList.add("nav__toggle--v");
+nav.classList.add("nav--closed");
+
+navToggle.addEventListener("click",function(evt){
+  evt.preventDefault();
+  nav.classList.toggle("nav--closed");
+});
+
